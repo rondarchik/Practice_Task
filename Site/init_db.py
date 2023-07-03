3aa521f58c78f941ad1c4ad0f90cfbb593410461
rondarchik <dovgun-viktoriya@mail.ru>
2023-07-04 00:45:04 +0300

    add elastic & update db filling logic

--- Site/init_db.py
+++ Site/init_db.py
@@ -1,4 +1,8 @@
+import csv
+import json
+import pandas
 import psycopg2
+import elasticsearch
 
 
 conn = psycopg2.connect(
@@ -8,9 +12,45 @@ conn = psycopg2.connect(
         password="password")
 
 
-def postgres_query():
-    """Create table in Postgres"""
+with open("config.json", "r") as file:
+    config = json.load(file)
+
+es = elasticsearch.Elasticsearch(
+    hosts=["http://127.0.0.1:9200"],
+    basic_auth=(config['elasticsearch']['user'], config['elasticsearch']['password'])
+)
+INDEX_NAME = 'posts'
+
+
+def insert_to_elastic():
+    body = {
+        'mappings': {
+            'properties': {
+                'id': {'type': 'integer'},
+                'text': {'type': 'text'}
+            }
+        }
+    }
+    es.indices.delete(index=INDEX_NAME)
+    es.indices.create(index=INDEX_NAME, body=body)
+
     cur = conn.cursor()
+    cur.execute("SELECT id, text FROM posts;")
+
+    for row in cur.fetchall():
+        id = row[0]
+        text = row[1]
+
+        body = {
+            'id': id,
+            'text': text
+        }
+        es.index(index=INDEX_NAME, body=body)
+
+
+def insert_to_postgres():
+    cur = conn.cursor()
+    cur.execute("set client_encoding='UTF-8';")
 
     cur.execute("DROP TABLE IF EXISTS posts;")
     cur.execute("""
@@ -22,18 +62,23 @@ def postgres_query():
         );
     """)
 
+    with open('posts.csv', 'r', encoding="utf-8") as file:
+        reader = csv.reader(file)
+        next(reader)
+
+        for row in reader:
+            query = u'INSERT INTO posts (text, created_date, rubrics) VALUES  (%s, %s, %s);'
+            cur.execute(query, (row[0], row[1], row[2]))
+
     conn.commit()
+    print('successfully')
 
 
 def main():
-    postgres_query()
+    insert_to_postgres()
+    insert_to_elastic()
 
 
 if __name__ == "__main__":
     main()
 
-
-# conn.commit()
-
-# cur.close()
-# conn.close()
