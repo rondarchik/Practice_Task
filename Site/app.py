4919862a1f1ea9132889990bc4e726f3db815dc1
rondarchik <dovgun-viktoriya@mail.ru>
2023-07-04 19:12:16 +0300

    add delete by id

--- Site/app.py
+++ Site/app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash, url_for
 import elasticsearch
 import psycopg2
 
@@ -42,11 +42,6 @@ def index():  # put application's code here
     return render_template('index.html', posts=posts)
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
 @app.route('/search', methods=['POST'])
 def search():
     text = request.form['search']
@@ -76,7 +71,7 @@ def search():
     cur = conn.cursor()
 
     if id_lists:
-        query = "SELECT text, created_date, rubrics FROM posts WHERE id IN (%s)" % (', '.join(str(id) for id in id_lists))
+        query = "SELECT text, created_date, rubrics FROM posts WHERE id IN (%s)"%(', '.join(str(id) for id in id_lists))
         query += " ORDER BY created_date;"
 
         cur.execute(query)
@@ -88,6 +83,51 @@ def search():
     conn.close()
 
     return render_template('search.html', posts=posts)
+
+
+def search_by_id(id):
+    body = {
+        "size": 1,
+        "query": {
+            "match": {
+                "id": id
+            }
+        }
+    }
+
+    result = es.search(index=INDEX_NAME, body=body)["hits"]["hits"]
+
+    if len(result) == 0:
+        return None
+
+    return result
+
+
+def delete_by_id(id):
+    try:
+        es.delete(index=INDEX_NAME, id=id)
+        return True
+    except elasticsearch.exceptions.NotFoundError:
+        return False
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    id = request.form['id']
+    delete_post = search_by_id(id)
+
+    if delete_post:
+        delete_by_id(delete_post[0]['_id'])
+        cur.execute("DELETE FROM posts WHERE id=%s", (id))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
